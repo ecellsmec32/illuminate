aa9374ff171729fc2b00bebb78c436d599cb2faa
infix <indrakshith.reddy@gmail.com>
2025-10-03 13:14:41 +0530

    Use HTTPS domain for backend

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:8000'
-        : 'http://44.223.11.89',  // Use port 80 (Nginx)
+        : 'https://clgserver.duckdns.org',  // HTTPS domain!
     
     APP_NAME: 'QrFlow',
     VERSION: '1.0.0'
