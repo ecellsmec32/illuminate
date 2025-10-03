580731576804969aaf53ebeef5693d89dafdd551
infix <indrakshith.reddy@gmail.com>
2025-10-03 17:02:28 +0530

    Use nyxgenai.com root domain for API

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,14 @@
 const CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:8000'
-        : 'https://clgserver.duckdns.org',  // HTTPS domain!
+        : 'https://nyxgenai.com',  // Root domain
     
     APP_NAME: 'QrFlow',
     VERSION: '1.0.0'
 };
 
 console.log('🔗 QrFlow API URL:', CONFIG.API_BASE_URL);
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
